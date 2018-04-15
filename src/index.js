f0dfee118bedc8a6b82e46d73a90d2d20e35be2b
hebo <1807941596@qq.com>
2018-04-15 23:48:15 +0800

    link 第三方图标库

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
   TabBarIOS,
-  Icon,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'; // 第三方图标库,需要rnpm link一下
 
 import Account from './page/Account';
 import Edit from './page/Edit';
 import List from './page/List';
 
 
-export default class App extends Component {
+export default class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -49,13 +49,15 @@ export default class App extends Component {
         unselectedItemTintColor='#000' // 当前没有被选中标签图标以及文字的颜色，ios10以上
         barTintColor='yellow' // 标签栏背景颜色
       >
-        <TabBarIOS.Item
-          title='list' // 图标下方小标题
+        <Icon.TabBarItem  // 用Icon.TabBarItem代替原生的TabBarIOS.Item
+          title='list'
+          iconName='ios-videocam-outline'
+          selectedIconName='ios-videocam' // 选中时候的图片，没有outline代表不透明了
           selected={selectedTab === 'list'} // 当前是否选中
           onPress={() => { this.tabOnPress(0) }}
         >
           <List />
-        </TabBarIOS.Item>
+        </Icon.TabBarItem>
         <TabBarIOS.Item
           title='edit'
           selected={selectedTab === 'edit'}
